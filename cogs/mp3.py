91a4131f1bda681dee849c68351092c471d9dcd7
Yeha <96789742+Yeha56@users.noreply.github.com>
2022-02-28 23:32:08 +0100

    Partially prevent blocking

--- cogs/mp3.py
+++ cogs/mp3.py
@@ -1,31 +1,48 @@
 from discord.ext import commands
 import discord
 from pytube import YouTube
-import os
+import io
 
 
 class Convert(commands.Cog):
-
-    def __init__(self, client):
+    def __init__(self, client: discord.Client):
         self.client = client
+        self.execute: callable = self.client.loop.run_in_executor
 
     @commands.command()
-    async def mp3(self, ctx, *, url):
-        with ctx.typing():
+    @commands.cooldown(1, 10, commands.BucketType.user)
+    async def mp3(self, ctx: commands.Context, *, url):
+        async with ctx.typing():
 
-            url = YouTube(url)
+            def get_buffer():
+                # Get audio stream
+                audio_stream = YouTube(url).streams.get_audio_only()
 
-            filename = str(url.streams.get_audio_only().download())
-            filepath_edited = filename.removesuffix(".mp4") + ".mp3"
+                # Check file size
+                if audio_stream.filesize_approx > 8_388_608:
+                    return None
 
-            os.rename(filename, filepath_edited)
+                # Download
+                buffer = io.BytesIO()
+                audio_stream.stream_to_buffer(buffer)
+                buffer.seek(0)
+                return buffer, audio_stream.title
+
+            # Gets the buffer in separate thread
+            if result := await self.execute(None, get_buffer):
+                buffer, title = result
+            else:
+                # File too big
+                await ctx.reply("File is over 8mb and cannot be sent.")
+
             try:
-                await ctx.reply(file=discord.File(filepath_edited))
+                await ctx.reply(file=discord.File(fp=buffer, filename=title + ".mp3"))
 
+            # This shouldn't happen
             except discord.HTTPException:
-                await ctx.reply("File too big.")
+                await ctx.reply("HTTP Error.")
 
-        os.remove(filepath_edited)
+            buffer.close()
 
 
 def setup(client):
