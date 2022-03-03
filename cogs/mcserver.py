d58ad2708f66bc58d66c5a9fff7f8af4a9f32ad0
Yeha <96789742+Yeha56@users.noreply.github.com>
2022-03-03 21:37:30 +0100

    Prevent blocking and other

--- cogs/mcserver.py
+++ cogs/mcserver.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
-import json
 
+import json
 with open("whitelist.json") as d:
     data = json.load(d)
 
@@ -37,13 +37,11 @@ def is_owner():
 
 
 class Mcserver(commands.Cog):
-    def __init__(self, client):
+    def __init__(self, client: commands.Bot):
         self.client = client
+        self.execute = self.client.loop.run_in_executor
 
-    global data
-
-    @commands.Cog.listener()
-    async def on_command_error(self, ctx: commands.Context, error):
+    async def cog_command_error(self, ctx: commands.Context, error):
         if isinstance(error, NotWhitelisted):
             await ctx.reply("You are not whitelisted.")
 
@@ -51,7 +49,7 @@ class Mcserver(commands.Cog):
     async def mcserver(self, ctx):
         pass
 
-    @mcserver.command(clean_params=True)
+    @mcserver.command()
     @is_owner()
     async def add(self, ctx: commands.Context, *, user: commands.MemberConverter):
         """Adds user to whitelist"""
@@ -73,7 +71,7 @@ class Mcserver(commands.Cog):
         if isinstance(error, commands.CheckFailure):
             await ctx.reply("You don't have permission to use this command.")
 
-    @mcserver.command(clean_params=True)
+    @mcserver.command()
     @is_whitelisted()
     async def remove(self, ctx: commands.Context, *, user: discord.Member = None):
         """Removes user from whitelist"""
@@ -94,38 +92,38 @@ class Mcserver(commands.Cog):
         if isinstance(error, commands.MissingRequiredArgument):
             await ctx.reply("Missing user to whitelist.")
 
-    @mcserver.command(clean_params=True)
+    @mcserver.command()
     @is_whitelisted()
     async def start(self, ctx: commands.Context):
         """Starts minecraft server"""
-        exa.start("4ksE0Nc5qCNb7ZYn")
+        await self.execute(None, exa.start, "4ksE0Nc5qCNb7ZYn")
 
         await ctx.reply("Server started.")
 
-    @mcserver.command(clean_params=True)
+    @mcserver.command()
+    @is_whitelisted()
+    async def execute(self, ctx, *, cmd):
+        """Execute a command on the server"""
+        await self.execute(None, exa.command, server_key, cmd)
+
+        await ctx.reply(f"Executed `{cmd}`.")
+
+    @mcserver.command(enabled=True)
+    @commands.cooldown(1, 10, commands.BucketType.user)
+    async def say(self, ctx, *, text):
+        """ Public say command """
+        await self.execute(None, exa.command, server_key,
+                           'tellraw @a [{"bold":true, "text":"%s "}, ' % ctx.author.display_name +
+                           '{"bold":false, "text":"says: %s"}]' % text
+                           )
+
+        await ctx.reply(f"Said `{text}`.")
+
+    @mcserver.command()
     @is_whitelisted()
     async def list(self, ctx):
         """Raw current whitelist"""
         await ctx.reply(f"Current raw whitelist: `{str(data['whitelisted'])}`")
-
-    @mcserver.command(clean_params=True)
-    @is_whitelisted()
-    async def execute(self, ctx, *, cmd):
-        """Execute a command on the server"""
-        # TODO: make async
-        exa.command(server_key, cmd)
-
-        await ctx.reply(f"Executed `{cmd}`.")
-
-    @mcserver.command(clean_params=True)
-    async def say(self, ctx, *, text):
-        # TODO: make async
-        exa.command(server_key,
-                    'tellraw @a [{"bold":true, "text":"%s "}, ' % ctx.author.display_name +
-                    '{"bold":false, "text":"says: %s"}]' % text
-                    )
-
-        await ctx.reply(f"Said `{text}`.")
 
 
 def setup(client):
