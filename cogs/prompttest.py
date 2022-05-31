bb0fa0ff0a787146a6eb517d68737e0719efc6f1
Yeha <96789742+Yeha56@users.noreply.github.com>
2022-05-31 07:13:28 +0200

    Add slash command test cog

--- cogs/prompttest.py
+++ cogs/prompttest.py
@@ -1,20 +1,30 @@
 import discord
 from discord.ext import commands
-from discord import ui
+from discord import ui, app_commands
+from config import GUILD
 
-import asyncio
+
+def check_if_it_is_me(interaction: discord.Interaction) -> bool:
+    return interaction.user.id == 150560836971266048
 
 
 class PromptTest(commands.Cog):
     def __init__(self, client):
-        self.client = client
+        self.client: commands.Bot = client
+        self.client.tree.add_command(self.prompt_test, guild=discord.Object(id=GUILD))
 
-    @commands.command(hidden=True, aliases=["prompt", "test"])
-    @commands.is_owner()
-    async def prompt_test(self, ctx: commands.Context):
-        await asyncio.sleep(10)
-        await ctx.reply("Done.")
+    @app_commands.command(name="prompt", description="Testing application command.")
+    @app_commands.describe(test_var="A variable used to test inputs.")
+    @app_commands.rename(test_var="test-variable")
+    @app_commands.check(check_if_it_is_me)
+    async def prompt_test(self, interaction: discord.Interaction, test_var: str = "placeholder"):
+        await interaction.response.send_message("This is a test\n" + test_var,
+                                                ephemeral=True)
 
 
 async def setup(client):
     await client.add_cog(PromptTest(client))
+
+
+async def teardown(client: commands.Bot):
+    client.tree.remove_command("prompt")
