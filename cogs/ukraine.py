25d9bec0faca758484b176321cbcae5a791b7b2a
Yeha <96789742+Yeha56@users.noreply.github.com>
2022-06-24 04:11:36 +0200

    Partially rework ukraine.py

--- cogs/ukraine.py
+++ cogs/ukraine.py
@@ -1,19 +1,26 @@
-from discord.ext import commands
+from __future__ import annotations
+
 import discord
+from discord import app_commands
+from discord.ext import commands
+from config import GUILD_OBJECT
 
 import io
-from typing import Union
+from typing import Union, Tuple, Optional, TYPE_CHECKING
 from PIL import Image, ImageDraw, ImageFilter
+
+if TYPE_CHECKING:
+    from main import MoistBot
 
 FLAG_UA = Image.open("./assets/Ukraine flag.png").convert("RGBA")
 
 
 class ImageGen:
-    def __init__(self, avatar: Image.Image, ring: Union[int, float] = 7,  upscale: int = 4):
-        self.avatar = avatar
-        self.flag_ua = FLAG_UA.copy()
-        self.RING = ring
-        self.UPSCALE = upscale
+    def __init__(self, avatar: Image.Image, ring: Union[int, float] = 7, upscale: int = 4):
+        self.avatar: Image.Image = avatar
+        self.flag_ua: Image.Image = FLAG_UA.copy()
+        self.RING: Union[int, float] = ring
+        self.UPSCALE: int = upscale
 
         # Original resolution
         self.width, self.height = self.avatar.size
@@ -21,69 +28,93 @@ class ImageGen:
 
         # Upscaled resolution to smooth ellipse mask
         self.u_width, self.u_height = self.width * self.UPSCALE, self.height * self.UPSCALE
-        self.u_ring_w, self.u_ring_h = (self.u_width * self.RING) / 100, (self.u_height * self.RING) / 100  # Ring bounds
+        self.u_ring_w, self.u_ring_h = (self.u_width * self.RING) / 100, (self.u_height * self.RING) / 100
+        # Ring bounds
 
         # Rounded
         self.r_ring_w, self.r_ring_h = round(self.ring_w), round(self.ring_h)
         self.r_width, self.r_height = (self.width - self.r_ring_w), (self.height - self.r_ring_h)
 
-        # Upscaled rounded
-        self.s_width, self.s_height = (self.r_width * self.UPSCALE), (self.r_height * self.UPSCALE)
+        # Upscale rounded
+        self.ru_width, self.ru_height = (self.r_width * self.UPSCALE), (self.r_height * self.UPSCALE)
 
         # Resize if needed
-        if self.avatar.size != self.flag_ua.size:
+        if self.flag_ua.size != self.avatar.size:
             self.flag_ua = self.flag_ua.resize(self.avatar.size, Image.BICUBIC)
 
-    def gen_mask(self, descale: Union[int, float] = 0) -> Image.Image:
-        """ Generate a smooth ellipse mask """
+    @staticmethod
+    def _gen_mask(
+            *,
+            start_size: Tuple[int, int],
+            x1y1: Tuple[int, int],
+            x2y2: Tuple[int, int],
+            final_size: Tuple[int, int],
+            descale: Optional[Union[int, float]] = 0
+    ) -> Image.Image:
+        """Generate a smooth ellipse mask"""
 
         # Upscaled ellipse mask
-        mask = Image.new("L", (self.s_width, self.s_height), 0)
+        mask = Image.new("L", start_size, 0)
         draw = ImageDraw.Draw(mask)
-        draw.ellipse((0 + descale, 0 + descale, self.s_width - descale, self.s_height - descale), fill=255)
+        draw.ellipse((x1y1[0] + descale, x1y1[1] + descale, x2y2[0] - descale, x2y2[1] - descale), fill=255)
 
-        mask = mask.resize((self.r_width, self.r_height), Image.BICUBIC)  # Downscale to smooth ellipse mask
+        mask = mask.resize(final_size, Image.BICUBIC)  # Downscale to smooth ellipse mask
         mask = mask.filter(ImageFilter.SMOOTH)  # Smooth edges
         return mask
 
     def proportional(self) -> Image.Image:
-        """ Proportional ring generator """
+        """Proportional ring generator"""
 
         masked_avatar = Image.new("RGBA", (self.r_width, self.r_height), (255, 255, 255, 0))
         avatar = self.avatar.resize((self.r_width, self.r_height), Image.LANCZOS)
-        masked_avatar = Image.composite(avatar, masked_avatar, mask=self.gen_mask())
 
-        self.flag_ua.paste(masked_avatar, box=(round(self.ring_w / 2), round(self.ring_h / 2)),
-                           mask=self.gen_mask(4))
+        masked_avatar = Image.composite(
+            avatar,
+            masked_avatar,
+            mask=self._gen_mask(
+                start_size=(self.ru_width, self.ru_height),
+                x1y1=(0, 0),
+                x2y2=(self.ru_width, self.ru_width),
+                final_size=(self.r_width, self.r_height)
+            )
+        )
+
+        self.flag_ua.paste(
+            masked_avatar,
+            box=(round(self.ring_w / 2), round(self.ring_h / 2)),
+            mask=self._gen_mask(
+                start_size=(self.ru_width, self.ru_height),
+                x1y1=(0, 0),
+                x2y2=(self.ru_width, self.ru_width),
+                final_size=(self.r_width, self.r_height),
+                descale=4
+            )
+        )
 
         return self.flag_ua
 
     def subtractive(self) -> Image.Image:
-        """ Subtractive ring generator """
+        """Subtractive ring generator"""
 
-        # Circle mask for the avatar
-        mask = Image.new("L", (self.u_width, self.u_height), 0)
-        draw = ImageDraw.Draw(mask)
-        draw.ellipse((self.u_ring_w, self.u_ring_h, self.u_height - self.u_ring_w, self.u_height - self.u_ring_h),
-                     fill=255)
-
-        # Downscale to smooth ellipse mask
-        mask = mask.resize(self.avatar.size, Image.LANCZOS)
-        mask = mask.filter(ImageFilter.SMOOTH)  # Smooth edges
-
-        img = Image.composite(self.avatar, self.flag_ua, mask)
+        img = Image.composite(self.avatar, self.flag_ua,
+                              mask=self._gen_mask(
+                                  start_size=(self.u_width, self.u_height),
+                                  x1y1=(self.u_ring_w, self.u_ring_h),
+                                  x2y2=(self.u_height - self.u_ring_w, self.u_height - self.u_ring_h),
+                                  final_size=self.avatar.size
+                                  )
+                              )
         return img
 
 
 class Ukraine(commands.Cog):
-    def __init__(self, client):
-        self.client = client
+    def __init__(self, client: MoistBot):
+        self.client: MoistBot = client
         self.execute = self.client.loop.run_in_executor
 
     # Method called by discord.py
-    async def cog_command_error(self, ctx: commands.Context,  error):
-        if isinstance(error, commands.MemberNotFound):
-            await ctx.reply(f"User `{error.argument}` not found")
+    async def cog_command_error(self, ctx: commands.Context, error):
+        pass
 
     @staticmethod
     def _get_buffer(avatar) -> io.BytesIO:
@@ -93,14 +124,17 @@ class Ukraine(commands.Cog):
         buffer.seek(0)
         return buffer
 
-    @commands.command(enabled=True)
-    async def ukraine(self, ctx: commands.Context, *, user: str = None):
+    @commands.hybrid_command()
+    @app_commands.guilds(GUILD_OBJECT)
+    @app_commands.describe(user="Optionally select a user")
+    async def ukraine(self, ctx: commands.Context, *, user: discord.User = commands.Author):
+        """Generates a Ukrainian colored ring border around someone's avatar"""
+
         async with ctx.typing():
-            user = await commands.MemberConverter().convert(ctx, user) if user else ctx.author
             try:
                 # Get avatar
-                avatar = await user.display_avatar.with_format("png").with_size(2048).read()
-                avatar = Image.open(io.BytesIO(avatar))
+                avatar: bytes = await user.display_avatar.with_format("png").with_size(2048).read()
+                avatar: Image.Image = Image.open(io.BytesIO(avatar))
 
                 # Avoid blocking
                 img_buffer = await self.execute(None, self._get_buffer, avatar)
@@ -109,9 +143,15 @@ class Ukraine(commands.Cog):
                 await ctx.reply(file=discord.File(fp=img_buffer, filename="image.png"))
 
             finally:
+                # Free memory
                 img_buffer.close()
                 avatar.close()
+                del img_buffer, avatar
 
 
 async def setup(client):
     await client.add_cog(Ukraine(client))
+
+
+async def teardown(client):
+    client.tree.remove_command("ukraine")
