861479900b5627354bc5a6d2813c68eb051f93f1
Yeha <96789742+Yeha56@users.noreply.github.com>
2023-08-27 00:46:13 +0200

    avatar: cleanup

--- cogs/avatar.py
+++ cogs/avatar.py
@@ -6,6 +6,8 @@ from typing import TYPE_CHECKING, Annotated, Union
 
 if TYPE_CHECKING:
     from main import MoistBot
+    from utils.context import Context
+
 
 class AvatarEmbed(discord.Embed):
     def __init__(
@@ -13,17 +15,14 @@ class AvatarEmbed(discord.Embed):
         avatar_url: str,
         user: Union[discord.User, discord.Member],
         *,
-        source: str = 'base'
+        source: str = 'base',
     ):
-        super().__init__(
-            type='image',
-            color=discord.Color.random(),
-        )
+        super().__init__(type='image', color=discord.Color.random())
         self.set_image(url=avatar_url)
         self.set_author(
-            name="%s's %s avatar" % (user.display_name, source),
+            name=f'{user.display_name}\'s {source} avatar',
             url=avatar_url,
-            icon_url=avatar_url
+            icon_url=avatar_url,
         )
 
 
@@ -31,30 +30,34 @@ class Avatar(commands.Cog):
     def __init__(self, client):
         self.client: MoistBot = client
 
+    @property
+    def display_emoji(self) -> discord.PartialEmoji:
+        return discord.PartialEmoji(name='\U0001f465')
+
     @commands.cooldown(rate=1, per=2, type=commands.BucketType.user)
     @commands.command()
     async def avatar(
         self,
         ctx: commands.Context,
         *,
-        user: Annotated[Union[discord.User, discord.Member], commands.MemberConverter] = commands.Author
+        user: Annotated[Union[discord.User, discord.Member], commands.MemberConverter] = commands.Author,
     ):
         """Display a user's avatar."""
 
-        has_ga: bool = hasattr(user, 'guild_avatar') and user.guild_avatar
-        embeds: list[discord.Embed] = []
-
         # user: discord.Member
-        # Second embed with guild avatar (if exists)
-        if has_ga:
-            avatar_url = user.guild_avatar.url
-            embed_guild = AvatarEmbed(avatar_url, user, source='guild')
-            embeds.append(embed_guild)
+        embeds: list[discord.Embed] = []
 
         # First embed with base avatar
         avatar_url = user.avatar.url
-        embed_base = AvatarEmbed(avatar_url, user)
-        embeds.insert(0, embed_base)
+        embed = AvatarEmbed(avatar_url, user)
+        embeds.append(embed)
+
+        # Second embed with guild avatar (if exists)
+        has_ga: bool = hasattr(user, 'guild_avatar') and user.guild_avatar
+        if has_ga:
+            avatar_url = user.guild_avatar.url
+            embed = AvatarEmbed(avatar_url, user, source='guild')
+            embeds.append(embed)
 
         # Send embeds
         await ctx.reply(embeds=embeds)
