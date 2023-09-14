aa6dd076ebc235d489f2b05157f35833886b5397
Yeha <96789742+Yeha56@users.noreply.github.com>
2023-09-14 23:30:48 +0200

    owner_only: actually fr this time add eval and repl

--- cogs/owner_only.py
+++ cogs/owner_only.py
@@ -5,10 +5,16 @@ import discord.utils
 from discord.ext import commands
 from config import GUILD_OBJECT
 
+import io
 import os
+import asyncio
+import inspect
 import logging
+import textwrap
+import traceback
 from yarl import URL
-from typing import TYPE_CHECKING, Optional, Literal, Annotated
+from contextlib import redirect_stdout
+from typing import TYPE_CHECKING, Optional, Literal, Annotated, Any
 
 if TYPE_CHECKING:
     from main import MoistBot
@@ -16,6 +22,7 @@ if TYPE_CHECKING:
 
 
 logger = logging.getLogger('discord.' + __name__)
+# noinspection PyBroadException
 
 
 class OwnerOnly(commands.Cog):
@@ -234,6 +241,152 @@ class OwnerOnly(commands.Cog):
         emoji = discord.utils.get(ctx.guild.emojis, name=alias)
         await ctx.guild.delete_emoji(emoji)
         await ctx.reply(':white_check_mark: Deleted emoji.')
+
+    @commands.command(hidden=True, name='eval')
+    async def _eval(self, ctx: Context, *, body: str):
+        """Evaluates a code"""
+
+        """
+        I'm sorry but this was way too cool not to yoink :3
+        https://github.com/Rapptz/RoboDanny/blob/a52a212d1fff1024fb00c14b9e125071f87e0323/cogs/admin.py#L215C31-L215C31
+        """
+
+        env = {
+            'client': self.client,
+            'ctx': ctx,
+            'channel': ctx.channel,
+            'author': ctx.author,
+            'guild': ctx.guild,
+            'message': ctx.message,
+            '_': self._last_result,
+        }
+
+        env.update(globals())
+
+        body = self.cleanup_code(body)
+        stdout = io.StringIO()
+
+        to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
+
+        try:
+            exec(to_compile, env)
+        except Exception as e:
+            return await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
+
+        func = env['func']
+        try:
+            with redirect_stdout(stdout):
+                ret = await func()
+        except Exception as e:
+            value = stdout.getvalue()
+            await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+        else:
+            value = stdout.getvalue()
+            try:
+                await ctx.message.add_reaction('\u2705')
+            except Exception:
+                pass
+
+            if ret is None:
+                if value:
+                    await ctx.send(f'```py\n{value}\n```')
+            else:
+                self._last_result = ret
+                await ctx.send(f'```py\n{value}{ret}\n```')
+
+    @commands.command(hidden=True)
+    async def repl(self, ctx: Context):
+        """Launches an interactive REPL session."""
+
+        """
+        This is so cool I couldn't resist qwq
+        https://github.com/Rapptz/RoboDanny/blob/a52a212d1fff1024fb00c14b9e125071f87e0323/cogs/admin.py#L262
+        """
+
+        variables = {
+            'ctx': ctx,
+            'client': self.client,
+            'message': ctx.message,
+            'guild': ctx.guild,
+            'channel': ctx.channel,
+            'author': ctx.author,
+            '_': None,
+        }
+
+        if ctx.channel.id in self.sessions:
+            await ctx.send('Already running a REPL session in this channel. Exit it with `quit`.')
+            return
+
+        self.sessions.add(ctx.channel.id)
+        await ctx.send('Enter code to execute or evaluate. `exit()` or `quit` to exit.')
+
+        def check(m):
+            return m.author.id == ctx.author.id and m.channel.id == ctx.channel.id and m.content.startswith('`')
+
+        while True:
+            try:
+                response = await self.client.wait_for('message', check=check, timeout=10.0 * 60.0)
+            except asyncio.TimeoutError:
+                await ctx.send('Exiting REPL session.')
+                self.sessions.remove(ctx.channel.id)
+                break
+
+            cleaned = self.cleanup_code(response.content)
+
+            if cleaned in ('quit', 'exit', 'exit()'):
+                await ctx.send('Exiting.')
+                self.sessions.remove(ctx.channel.id)
+                return
+
+            executor = exec
+            code = ''
+            if cleaned.count('\n') == 0:
+                # single statement, potentially 'eval'
+                try:
+                    code = compile(cleaned, '<repl session>', 'eval')
+                except SyntaxError:
+                    pass
+                else:
+                    executor = eval
+
+            if executor is exec:
+                try:
+                    code = compile(cleaned, '<repl session>', 'exec')
+                except SyntaxError as e:
+                    await ctx.send(self.get_syntax_error(e))
+                    continue
+
+            variables['message'] = response
+
+            fmt = None
+            stdout = io.StringIO()
+
+            try:
+                with redirect_stdout(stdout):
+                    result = executor(code, variables)
+                    if inspect.isawaitable(result):
+                        result = await result
+            except Exception as e:
+                value = stdout.getvalue()
+                fmt = f'```py\n{value}{traceback.format_exc()}\n```'
+            else:
+                value = stdout.getvalue()
+                if result is not None:
+                    fmt = f'```py\n{value}{result}\n```'
+                    variables['_'] = result
+                elif value:
+                    fmt = f'```py\n{value}\n```'
+
+            try:
+                if fmt is not None:
+                    if len(fmt) > 2000:
+                        await ctx.send('Content too big to be printed.')
+                    else:
+                        await ctx.send(fmt)
+            except discord.Forbidden:
+                pass
+            except discord.HTTPException as e:
+                await ctx.send(f'Unexpected error: `{e}`')
 
 
 async def setup(client: MoistBot):
