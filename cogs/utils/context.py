c3afb91a5a1e4998737abbd06a6646245a1f1d2e
Yeha <96789742+Yeha56@users.noreply.github.com>
2023-10-05 15:33:28 +0200

    Add `prompt()` to ctx

--- cogs/utils/context.py
+++ cogs/utils/context.py
@@ -9,6 +9,44 @@ if TYPE_CHECKING:
     from main import MoistBot
 
 
+class ConfirmationView(discord.ui.View):
+    def __init__(self, *, timeout: float, author_id: int, delete_after: bool) -> None:
+        super().__init__(timeout=timeout)
+        self.value: Optional[bool] = None
+        self.delete_after: bool = delete_after
+        self.author_id: int = author_id
+        self.message: Optional[discord.Message] = None
+
+    async def interaction_check(self, interaction: discord.Interaction) -> bool:
+        if interaction.user and interaction.user.id == self.author_id:
+            return True
+        else:
+            await interaction.response.send_message('This confirmation dialog is not for you.', ephemeral=True)
+            return False
+
+    async def on_timeout(self) -> None:
+        if self.delete_after and self.message:
+            await self.message.delete()
+
+    @discord.ui.button(label='Confirm', style=discord.ButtonStyle.green)
+    async def confirm(self, interaction: discord.Interaction, button: discord.ui.Button):
+        self.value = True
+        await interaction.response.defer()
+        if self.delete_after:
+            await interaction.delete_original_response()
+
+        self.stop()
+
+    @discord.ui.button(label='Cancel', style=discord.ButtonStyle.red)
+    async def cancel(self, interaction: discord.Interaction, button: discord.ui.Button):
+        self.value = False
+        await interaction.response.defer()
+        if self.delete_after:
+            await interaction.delete_original_response()
+
+        self.stop()
+
+
 class Context(commands.Context):
     prefix: str
     command: commands.Command[Any, ..., Any]
@@ -46,3 +84,45 @@ class Context(commands.Context):
         if label is not None:
             return f'{emoji}: {label}'
         return emoji
+
+    async def prompt(
+        self,
+        message: str,
+        *,
+        timeout: float = 60.0,
+        delete_after: bool = True,
+        author_id: Optional[int] = None,
+    ) -> Optional[bool]:
+        """An interactive reaction confirmation dialog.
+
+        github.com/Rapptz/RoboDanny/blob/rewrite/cogs/utils/context.py#L198C22-L198C22
+
+        Parameters
+        -----------
+        message: str
+            The message to show along with the prompt.
+        timeout: float
+            How long to wait before returning.
+        delete_after: bool
+            Whether to delete the confirmation message after we're done.
+        author_id: Optional[int]
+            The member who should respond to the prompt. Defaults to the author of the
+            Context's message.
+
+        Returns
+        --------
+        Optional[bool]
+            ``True`` if explicit confirm,
+            ``False`` if explicit deny,
+            ``None`` if deny due to timeout
+        """
+
+        author_id = author_id or self.author.id
+        view = ConfirmationView(
+            timeout=timeout,
+            delete_after=delete_after,
+            author_id=author_id,
+        )
+        view.message = await self.send(message, view=view, ephemeral=delete_after)
+        await view.wait()
+        return view.value
